a8a8b74b82bb21fad8e405912968fd89b2d7a8bb
Renato Diniz <dinizre@hotmail.com>
2022-11-28 18:24:49 -0300

    Desafio 02 - Trabalhando com middlewares

---
+++ README.md
@@ -0,0 +1,20 @@
+## Todo - Desafio Ignite Middlewares
+
+### Requisitos
+
+- [x] Criar usuario com name, username, id (uuid) e todo[]
+- [x] Criar novo todo
+- [x] Listar todos os todo
+- [x] Alterar titulo e deadline de um todo existente
+- [x] Marcar todos os todo com feito
+- [x] Excluir um todo
+
+### Regras de negócio
+
+- [x] Não deve ser possível criar user com username já existente
+- [x] Não deve ser possível atualizar um todo não existente
+- [x] Não deve ser possível deletar um todo não existente
+- [x] Verificar se existe user com o usernames
+- [x] Verificar se pode incluir todo conforme o plano
+- [x] Validar ID
+- [x] Verificar se um TODO pertence ao usuário informado
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, validate } = require('uuid');
+const { v4: uuidv4, validate, v4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +10,86 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username == username);
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not found'
+    })
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+
+  const { user } = request;
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({
+      error: 'Can\'t add more than 10 todos in user\'s plan'
+    });
+  }
+
+  request.user = user;
+
+  next();
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: 'Id is invalid!'
+    })
+  }
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not found'
+    })
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({
+      error: 'Todo isn\'t from this Username'
+    })
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id == id);
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not found'
+    })
+  }
+
+  request.user = user;
+
+  next();
+
 }
 
 app.post('/users', (request, response) => {
